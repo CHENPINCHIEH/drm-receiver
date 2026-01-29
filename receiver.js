12414b9babf5190c3a4edbb02132c44c49f61132
CHENPINCHIEH <f2828282828@gmail.com>
2026-01-29 09:59:08 +0000

    change to google ads

--- receiver.js
+++ receiver.js
@@ -44,7 +44,7 @@ try {
         streamType: 'BUFFERED',
         title: 'DRM + Custom VMAP Test',
         vmapAdsRequest: {
-          adTagUrl: HOSTED_VMAP_URL
+          adTagUrl: 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpreonly&cmsid=496&vid=short_onecue&correlator='
         },
         stitchedContentTimeline: true
       };
