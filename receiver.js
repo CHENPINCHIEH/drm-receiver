de16cf91587d7b46b802f6e75350e42eb1c0d0fa
CHENPINCHIEH <f2828282828@gmail.com>
2026-01-29 09:26:02 +0000

    add online vmap

--- receiver.js
+++ receiver.js
@@ -7,43 +7,10 @@ window.queuedCacLogs = window.queuedCacLogs || [];
 const MAIN_DRM_CONTENT_ID = 'https://storage.googleapis.com/shaka-demo-assets/angel-one-widevine/dash.mpd';
 const WIDEVINE_LICENSE_SERVER = 'https://cwip-shaka-proxy.appspot.com/no_auth';
 // 使用您剛剛測試成功、確定可播放的 MP4 連結
-const WORKING_AD_MP4 = 'https://storage.googleapis.com/interactive-media-ads/media/android.mp4';
+// const WORKING_AD_MP4 = 'https://storage.googleapis.com/interactive-media-ads/media/android.mp4';
+const HOSTED_VMAP_URL = 'https://chenpinchieh.github.io/cast_sender_sample/custom_vmap.xml';
 
-// --- 建構自定義 VMAP XML (指向會動的 MP4) ---
-// 這樣可以繞過外部廣告伺服器的不穩定因素
-const CUSTOM_VMAP_XML = `
-<vmap:VMAP xmlns:vmap="http://www.iab.net/videosuite/vmap" version="1.0">
-  <vmap:AdBreak timeOffset="start" breakType="linear" breakId="preroll">
-    <vmap:AdSource id="preroll-ad-source" allowMultipleAds="false" followRedirects="true">
-      <vmap:VASTAdData>
-        <VAST version="3.0">
-          <Ad id="1">
-            <InLine>
-              <AdSystem>Test</AdSystem>
-              <AdTitle>Working MP4 Ad</AdTitle>
-              <Creatives>
-                <Creative>
-                  <Linear>
-                    <Duration>00:00:10</Duration>
-                    <MediaFiles>
-                      <MediaFile delivery="progressive" type="video/mp4" width="640" height="360">
-                        <![CDATA[${WORKING_AD_MP4}]]>
-                      </MediaFile>
-                    </MediaFiles>
-                  </Linear>
-                </Creative>
-              </Creatives>
-            </InLine>
-          </Ad>
-        </VAST>
-      </vmap:VASTAdData>
-    </vmap:AdSource>
-  </vmap:AdBreak>
-</vmap:VMAP>
-`.trim();
 
-// 將 XML 轉換為 Data URI，讓 Receiver 把它當作一個網址讀取
-const VMAP_DATA_URI = 'data:text/xml;charset=utf-8,' + encodeURIComponent(CUSTOM_VMAP_XML);
 
 
 // --- Logging for CaC Tool ---
@@ -77,7 +44,7 @@ try {
         streamType: 'BUFFERED',
         title: 'DRM + Custom VMAP Test',
         vmapAdsRequest: {
-          adTagUrl: VMAP_DATA_URI // <--- 關鍵：使用 Data URI
+          adTagUrl: HOSTED_VMAP_URL
         },
         stitchedContentTimeline: true
       };
